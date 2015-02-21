30b52f8e6c952173bdb877f518db0938ba55142c
Danielle Siauw <danielle@daniellesiauw.com>
2015-02-21 18:56:21 +0800

    updates

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
 #Create a makeCacheMatrix
+#To run this you have to pass in a matricx e.g. A = matrix(c(2, 4, 3, 1, 5, 7,1,2,1),nrow=3,ncol=3,byrow=TRUE)
+#then parse the makeCacheMatrix(A) with the defined matrix A above into cacheSolve()
+#cacheSolve(makeCacheMatrix(A)) to get the inverse whether it is cached or not.
 makeCacheMatrix <- function(x = matrix()) {
-        m<-NULL   #set the inverse matrix as a Null matrix called m
+        m<-NULL   #intialise the inverse matrix as a Null matrix called m
         set<-function(y){  
                 x<<-y  #Create a set function to initialise the matrix
                 m<<-NULL #Create a set function to initialise the inverse matrix to NULL 
         }
-        print(x)
-        get<-function() x  #Create a get function to retrieve the value of the matrix
+        print(x) #print the initial matrix 
+        get<-function() x  #Create a get function to retrieve the value of the initial matrix
         setmatrix<-function(solve) m<<- solve  #Set the matrix to it's inverse
         getmatrix<-function() m  #Retrieve the inverse matrix
-        list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix) #Create a list of matrices that is inputted and reversed
+        list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix) #Create a list of functions that return the matrices that is inputted and reversed
         
 }
 
